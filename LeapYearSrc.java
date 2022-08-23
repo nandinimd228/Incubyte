4078ce53aa5bdf535f0fd7dea9a2ad50efbba616
nandinimd228 <108878812+nandinimd228@users.noreply.github.com>
2022-08-23 18:11:58 +0530

    Update LeapYearSrc.java

--- LeapYearSrc.java
+++ LeapYearSrc.java
@@ -1,13 +1,47 @@
-package LeapYear;
+package LeapYearTDD;
 
 public class LeapYearSrc {
 	int year;
 	
-	public static boolean findLeapYear(int year) {
+	public static boolean findLeapYear(int year){
 		
-		if (year % 400 == 0 || year % 4 == 0 && year % 100 != 0) {
+		if (year < 0) {
+			System.out.println(year);
+			String message = "A year cannot be negative: " + year + ". Did you mean " + Math.abs(year) + "?";
+			throw new IllegalArgumentException(message);
+		}
+		
+		if (yearDivisibleby400(year) || yearDivisibleBy4andNotBy100(year)) {
 			return true;
 		}
+		
+		return false;
+	}
+	
+	private static boolean yearDivisibleby400(int year) {
+		if(year % 400 == 0)
+			return true;
+		
+		return false;
+	}
+	
+	private static boolean yearDivisibleBy4andNotBy100(int year) {
+		if (yearDivisibleBy4(year) && yearNotDivisibleBy100(year))
+			return true;
+		
+		return false;
+	}
+	
+	private static boolean yearDivisibleBy4(int year) {
+		if (year % 4 == 0)
+			return true;
+		
+		return false;
+	}
+	
+	private static boolean yearNotDivisibleBy100(int year) {
+		if (year % 100 != 0)
+			return true;
 		
 		return false;
 	}
